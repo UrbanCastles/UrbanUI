49ca5440a9e6700796133c9e55003e46b1dbcc91
Reymark Urbano <reymark.urbano@infor.com>
2023-12-20 20:06:30 +0800

    Fixed Rendering Setups, Updated Path Iconer, and Modified Utilities

---
+++ src/UrbanUI.WPF/Controls/PathIconer/PathIconer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UrbanUI.WPF.Controls;
+
+public partial class PathIconer: Control
+{
+   #region DP: DataPath
+   public Geometry DataPath
+   {
+      get { return (Geometry)GetValue(DataPathProperty); }
+      set { SetValue(DataPathProperty, value); }
+   }
+   public static readonly DependencyProperty DataPathProperty = DependencyProperty.Register(nameof(DataPath), typeof(Geometry), typeof(PathIconer), new PropertyMetadata(null));
+   #endregion DP: DataPath
+
+   #region DP: Fill
+   public Brush Fill
+   {
+      get { return (Brush)GetValue(FillProperty); }
+      set { SetValue(FillProperty, value); }
+   }
+   public static readonly DependencyProperty FillProperty = DependencyProperty.Register(nameof(Fill), typeof(Brush), typeof(PathIconer), new PropertyMetadata(Brushes.White));
+   #endregion DP:Fill
+}
